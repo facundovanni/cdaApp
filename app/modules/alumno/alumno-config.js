b0194241df6d002a2d08cb809dcabc486ff3ea94
facundovanni <facundovanni@gmail.com>
2018-06-20 16:45:57 -0300

    Agregada carpeta modules
    Todos los modulos van en modules

--- app/modules/alumno/alumno-config.js
+++ app/modules/alumno/alumno-config.js
@@ -1,8 +1,8 @@
 // tag::module[]
 angular.module('cdApp.alumno', ['ngResource', 'ngMaterial', 'ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/alumno', {
-            templateUrl: 'alumno/alumno.html',
+        $routeProvider.when('/alumno/index', {
+            templateUrl: 'modules/alumno/alumno.html',
             controller: 'AlumnosController',
         });
     }])
